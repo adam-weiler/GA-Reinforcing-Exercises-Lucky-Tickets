21ad230508847db3fcd033a0f53a7b6638393c62
adam-weiler <adam.weiler@yahoo.ca>
2019-08-26 15:10:41 -0400

    Cleaning up code a bit.

--- exercise.js
+++ exercise.js
@@ -1,89 +1,51 @@
 const luckCheck = (str) => {
-    console.log(parseInt(str))
+    console.log(`Testing: ${str}`);
 
     if (isNaN(parseInt(str)) || (parseInt(str) < 1)) {
         return false;
     }
 
-
-
-
-    // let index = 0;
-    // let sum = 0;
-    console.log(str)
-    // console.log(str.length)
     let half = Math.floor(str.length / 2);
-    // console.log(half);
 
     let left = str.substring(0, half);
     let right = str.substring(str.length - half, str.length); 
-    console.log(left)
-    console.log(right)
+    console.log(`Left: ${left}`);
+    console.log(`Right: ${right}`);
 
     function sumHalf(digits) {
         let sum = 0;
-        // console.log(digits)
 
         for (let i = 0; i < digits.length; i++) {
             let digit = digits.slice(i, i+1);
-            // console.log(parseInt(digit))
+            // console.log(`Digit: ${digit}`);
             sum += parseInt(digit);
         }
-
         return sum;
-
-
-        // digits = parseInt(digits);
-        // console.log(digits);
-
-        // sum += digit;
-        // return sum
-
     }
 
     left = sumHalf(left);
     right = sumHalf(right);
 
-    console.log(left)
-    console.log(right)
+    console.log(`Left half: ${left}`);
+    console.log(`Right half: ${right}`);
 
-    if (left === right) {
-        return true;
-    } else {
-        return false;
-    }
-
-    // rSum(left);
-    // rSum(right);
-
-
-    // left.forEach(rSum())
-    // [...left].forEach(d => rSum(d))
-
-    
-    // Cut string in half ; check if odd or even
-    //Left half, right half
-        
-        //Convert to int
-        //Grab each digit
-        //Add altogether
-        //Compare
-    
+    return left === right ? true : false;
 }
 
 
-console.log(luckCheck("003111"));
+console.log(luckCheck("003111"));  // True.
 console.log();
-console.log(luckCheck("813372"));
+console.log(luckCheck("813372"));  // True.
 console.log();
-console.log(luckCheck("17935"));
+console.log(luckCheck("17935"));  // True.
 console.log();
-console.log(luckCheck("56328116"));
+console.log(luckCheck("56328116"));  // True.
 console.log();
-
 
 console.log(luckCheck("4"));  // Evaluates to 0:0, returns true.
 console.log();
 console.log(luckCheck("-4"));  // Less than 1 returns false.
 console.log();
 console.log(luckCheck("abce"));  // Letters return false.
+console.log();
+console.log(luckCheck(""));  // Blank return false.
